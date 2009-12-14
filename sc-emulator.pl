8993376bfa0215ff5f359d01ad8689dc3dc894aa
Dobrica Pavlinusic <dpavlin@rot13.org>
2009-12-15 00:57:18 +0100

    define $patron, $barcode and $loc(ation)

--- sc-emulator.pl
+++ sc-emulator.pl
@@ -8,6 +8,9 @@ use autodie;
 
 our $user     = 'sip2-user';
 our $password = 'sip2-paasswd';
+our $patron   = 200000000042;
+our $barcode  = 1301132799;
+our $loc      = 'FFZG';
 
 require 'config.pl' if -e 'config.pl';
 
@@ -35,11 +38,12 @@ sip2 "9300CN$user|CO$password|" => qr/^941/;
 # SC Status
 sip2 "9900302.00" => qr/^98/;
 
-# Checkin
-sip2 "09N20091214    08142820091214    081428AP|AOFFZG|AB1301036581|AC|BIN|" => qr/^10/;
-
 # Patron Information
-sip2 "6300020091214    085452          AOFFZG|AA200903260093|AC$password|" => qr/^64/;
+sip2 "6300020091214    085452          AO$loc|AA$patron|AC$password|" => qr/^64/;
 
 # Checkout
-sip2 "11YN20091214    124436                  AOFFZG|AA200905060034|AB1301132799|AC$password|BON|BIN|" => qw/12/;
+sip2 "11YN20091214    124436                  AO$loc|AA$patron|AB$barcode|AC$password|BON|BIN|" => qw/12/;
+
+# Checkin
+sip2 "09N20091214    08142820091214    081428AP|AO$loc|AB$barcode|AC|BIN|" => qr/^10/;
+
